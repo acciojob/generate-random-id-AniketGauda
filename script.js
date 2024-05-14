8052076ca344ecddb42785a8fca563f5558dcd2e
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-14 23:00:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let str="",p=10;
+    for(let i=0;i<l;i++){
+        let a = Math.floor(Math.random()*p)%128;
+        str+=String.fromCharCode(a);
+        p*=10;
+    }
+    return str;
 }
 
 // Do not change the code below.
